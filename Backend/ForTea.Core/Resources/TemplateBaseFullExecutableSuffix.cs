b6aa907ac03465764d3bb1d8dd3e31f03e9126ea
Kirill <kirillgla@gmail.com>
2019-07-24 10:22:16 +0300

    Update resources to match resources generated by VS

--- Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
+++ Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
@@ -1,7 +1,7 @@
-	public class TemplateBaseFullExecutableSuffix____GeneratedClass___LetsHopeNoOneEverNamesAClassLikeThat__IfYouNameClassLikeThatWeAreNotResponsibleForIncorrectBehaviour
-	{
-		public static void Main(string[] args)
-		{
-			File.WriteAllText(args[0], new $(PARAMETER_0)().TransformText(), Encoding.UTF8);
-		}
-	}
+    public class TemplateBaseFullExecutableSuffix____GeneratedClass___LetsHopeNoOneEverNamesAClassLikeThat__IfYouNameClassLikeThatWeAreNotResponsibleForIncorrectBehaviour
+    {
+        public static void Main(string[] args)
+        {
+            File.WriteAllText(args[0], new $(PARAMETER_0)().TransformText(), Encoding.UTF8);
+        }
+    }
