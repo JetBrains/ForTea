373366d1ffe22bb67be999c646a7c532727f9e93
Kirill Glazyrin <kirill_gla@mail.ru>
2019-08-12 21:33:19 +0300

    Merge tokens on frontend

--- Frontend/src/main/kotlin/com/jetbrains/fortea/highlighting/T4AnnotatorVisitor.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/highlighting/T4AnnotatorVisitor.kt
@@ -2,7 +2,6 @@ package com.jetbrains.fortea.highlighting
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.jetbrains.fortea.psi.T4AttributeName
-import com.jetbrains.fortea.psi.T4AttributeValue
 import com.jetbrains.fortea.psi.T4DirectiveName
 import com.jetbrains.fortea.psi.T4Visitor
 
@@ -13,9 +12,5 @@ class T4AnnotatorVisitor(private val holder: AnnotationHolder) : T4Visitor() {
 
   override fun visitAttributeName(attributeName: T4AttributeName) {
     holder.createInfoAnnotation(attributeName, null).textAttributes = T4SyntaxHighlighter.ATTRIBUTE_KEY
-  }
-
-  override fun visitAttributeValue(attributeValue: T4AttributeValue) {
-    holder.createInfoAnnotation(attributeValue, null).textAttributes = T4SyntaxHighlighter.ATTRIBUTE_VALUE
   }
 }
--- Frontend/src/main/kotlin/com/jetbrains/fortea/lexer/T4Lexer.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/lexer/T4Lexer.kt
@@ -1,5 +1,16 @@
 package com.jetbrains.fortea.lexer
 
 import com.intellij.lexer.FlexAdapter
+import com.intellij.lexer.MergingLexerAdapter
+import com.intellij.psi.tree.TokenSet
+import com.jetbrains.fortea.psi.T4ElementTypes
 
-class T4Lexer : FlexAdapter(_T4Lexer(null))
+class T4Lexer : MergingLexerAdapter(FlexAdapter(_T4Lexer(null)), tokensToMerge) {
+  companion object {
+    private val tokensToMerge = TokenSet.create(
+      T4ElementTypes.RAW_ATTRIBUTE_VALUE,
+      T4ElementTypes.RAW_CODE,
+      T4ElementTypes.RAW_TEXT
+    )
+  }
+}
