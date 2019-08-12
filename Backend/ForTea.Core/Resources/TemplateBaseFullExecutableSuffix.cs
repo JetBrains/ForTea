b09965ff57688afccbb44fc1601934195489c07b
Kirill Glazyrin <kirill_gla@mail.ru>
2019-08-12 18:27:51 +0300

    Fix broken executable suffix

--- Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
+++ Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
@@ -5,5 +5,6 @@
             string result = new $(PARAMETER_0)().TransformText();
             var encoding = Encoding.GetEncoding($(PARAMETER_1));
             File.WriteAllText(args[0], result, encoding);
+            return 0;
         }
     }
