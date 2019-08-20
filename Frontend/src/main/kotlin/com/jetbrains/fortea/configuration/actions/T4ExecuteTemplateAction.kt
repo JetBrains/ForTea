ae0c8694f1c6ff6201d3afda6277d20e795d1d6d
Kirill Glazyrin <kirillgla@gmail.com>
2019-08-20 18:51:04 +0300

    Improve context action wording

--- Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/actions/T4ExecuteTemplateAction.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/actions/T4ExecuteTemplateAction.kt
@@ -4,6 +4,6 @@ import com.intellij.execution.Executor
 import com.intellij.execution.executors.DefaultRunExecutor
 import com.jetbrains.rider.icons.ReSharperPsiBuildScriptsIcons
 
-class T4ExecuteTemplateAction : T4RunTemplateActionBase("Execute template", ReSharperPsiBuildScriptsIcons.Run) {
+class T4ExecuteTemplateAction : T4RunTemplateActionBase("Run template", ReSharperPsiBuildScriptsIcons.Run) {
   override val executor: Executor = DefaultRunExecutor.getRunExecutorInstance()
 }
