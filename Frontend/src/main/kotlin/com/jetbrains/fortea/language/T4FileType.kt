db187636d382aee186410aaa66a07cbb88a491e0
Kirill Glazyrin <kirillgla@gmail.com>
2019-08-20 10:36:02 +0300

    Highlight C# code with frontend lexer, too

--- Frontend/src/main/kotlin/com/jetbrains/fortea/language/T4FileType.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/language/T4FileType.kt
@@ -1,10 +1,19 @@
 package com.jetbrains.fortea.language
 
+import com.intellij.openapi.fileTypes.FileTypeEditorHighlighterProviders
+import com.intellij.openapi.fileTypes.TemplateLanguageFileType
+import com.jetbrains.fortea.highlighting.T4EditorSyntaxHighlighter
 import com.jetbrains.fortea.icons.T4Icons
 import com.jetbrains.rider.ideaInterop.fileTypes.RiderLanguageFileTypeBase
 import javax.swing.Icon
 
-object T4FileType : RiderLanguageFileTypeBase(T4Language) {
+object T4FileType : RiderLanguageFileTypeBase(T4Language), TemplateLanguageFileType {
+  init {
+    FileTypeEditorHighlighterProviders.INSTANCE.addExplicitExtension(this) { project, _, virtualFile, colors ->
+      T4EditorSyntaxHighlighter(project, virtualFile, colors)
+    }
+  }
+
   override fun getDefaultExtension() = "T4"
   override fun getDescription() = "T4 template file"
   override fun getIcon(): Icon = T4Icons.T4
