c42c6cbc84e1cbdde21e98c287b690dffe16a076
Kirill Glazyrin <kirillgla@gmail.com>
2019-08-20 12:02:28 +0300

    Reduce boilerplate in lexer

--- Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4UnexpectedDirectiveAnalyzer.cs
+++ Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4UnexpectedDirectiveAnalyzer.cs
@@ -1,23 +1,21 @@
 using GammaJul.ForTea.Core.Daemon.Highlightings;
-using GammaJul.ForTea.Core.Psi.Directives;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 
 namespace GammaJul.ForTea.Core.Daemon.ProblemAnalyzers
 {
-	[ElementProblemAnalyzer(typeof(IT4Directive), HighlightingTypes =
+	[ElementProblemAnalyzer(typeof(IT4UnknownDirective), HighlightingTypes =
 		new[] {typeof(T4UnexpectedDirectiveHighlighting)})]
-	public class T4UnexpectedDirectiveAnalyzer : ElementProblemAnalyzer<IT4Directive>
+	public class T4UnexpectedDirectiveAnalyzer : ElementProblemAnalyzer<IT4UnknownDirective>
 	{
 		protected override void Run(
-			IT4Directive element,
+			IT4UnknownDirective element,
 			ElementProblemAnalyzerData data,
 			IHighlightingConsumer consumer
 		)
 		{
 			var name = element.Name;
 			if (name == null) return;
-			if (T4DirectiveInfoManager.GetDirectiveByName(name.GetText()) != null) return;
 			consumer.AddHighlighting(new T4UnexpectedDirectiveHighlighting(name));
 		}
 	}
