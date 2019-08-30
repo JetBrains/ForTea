b508bef44333f8a11bdffcf374661efeed487e7f
Kirill Glazyrin <kirillgla@gmail.com>
2019-08-30 10:18:42 +0300

    Report errors and warnings in non-hostspecific templates, too

--- Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
+++ Backend/ForTea.Core/Resources/TemplateBaseFullExecutableSuffix.cs
@@ -3,8 +3,15 @@
         public static int Main(string[] args)
         {
             RegisterAssemblyLocations__Generated();
-            string result = new $(PARAMETER_0)().TransformText();
+            var transformation = new $(PARAMETER_0)();
+            string result = transformation.TransformText();
             var encoding = Encoding.GetEncoding($(PARAMETER_1));
+            foreach (global::System.CodeDom.Compiler.CompilerError error in transformation.Errors)
+            {
+                global::System.Console.Error.WriteLine(error);
+            }
+
+            if (transformation.Errors.HasErrors) return 1;
             File.WriteAllText(args[0], result, encoding);
             return 0;
         }
