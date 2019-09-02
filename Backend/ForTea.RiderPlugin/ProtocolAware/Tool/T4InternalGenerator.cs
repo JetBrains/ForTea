91de980d1b59df4cee3e561a0a8312bd9d57690e
Kirill Glazyrin <kirillgla@gmail.com>
2019-09-02 21:55:06 +0300

    Correctly handle preprocessed templates from the background, too

--- Backend/ForTea.RiderPlugin/ProtocolAware/Tool/T4InternalGenerator.cs
+++ Backend/ForTea.RiderPlugin/ProtocolAware/Tool/T4InternalGenerator.cs
@@ -1,23 +1,26 @@
 using System.Linq;
 using GammaJul.ForTea.Core.Psi;
 using GammaJul.ForTea.Core.Psi.FileType;
+using GammaJul.ForTea.Core.TemplateProcessing.CodeGeneration.Generators;
+using GammaJul.ForTea.Core.TemplateProcessing.Services;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
 using JetBrains.Application;
 using JetBrains.Application.Threading;
+using JetBrains.ForTea.RiderPlugin.TemplateProcessing.Managing;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Features.Altering.Resources;
 using JetBrains.ReSharper.Host.Features.ProjectModel.CustomTools;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Files;
 using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.UI.Icons;
-using JetBrains.Util;
 using UsageStatisticsNew = JetBrains.Application.ActivityTrackingNew.UsageStatistics;
 
 namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Tool
 {
-	[ShellComponent]
+	[ShellComponent] // This class cannot be made a solution component
 	public sealed class T4InternalGenerator : ISingleFileCustomTool
 	{
 		public string Name => "Bundled T4 template executor";
@@ -42,7 +45,10 @@ namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Tool
 		{
 			using (projectFile.Locks.UsingReadLock())
 			{
-				return projectFile.LanguageType.Is<T4ProjectFileType>();
+				if (!projectFile.LanguageType.Is<T4ProjectFileType>()) return false;
+				var dataManager = projectFile.GetSolution().GetComponent<IT4ProjectModelTemplateDataManager>();
+				var kind = dataManager.GetTemplateKind(projectFile);
+				return kind != T4TemplateKind.Unknown;
 			}
 		}
 
@@ -53,19 +59,33 @@ namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Tool
 		{
 			Statistics.TrackAction("T4.Template.Execution.Background");
 			var file = projectFile.ToSourceFile()?.GetPsiFiles(T4Language.Instance).OfType<IT4File>().SingleOrDefault();
-			var manager = file?.GetSolution().GetComponent<IT4TemplateExecutionManager>();
-			Execute(file, manager);
-			return new SingleFileCustomToolExecutionResult(
-				EmptyList<FileSystemPath>.Collection,
-				EmptyList<string>.Collection
-			);
+			if (file == null) return SingleFileCustomToolExecutionResult.NotExecuted;
+			var solution = file.GetSolution();
+			var dataManager = solution.GetComponent<IT4ProjectModelTemplateDataManager>();
+			var kind = dataManager.GetTemplateKind(projectFile);
+			if (kind is T4TemplateKind.Executable) Execute(file, solution);
+			if (kind is T4TemplateKind.Preprocessed) Preprocess(file, solution);
+			return SingleFileCustomToolExecutionResult.NotExecuted;
 		}
 
-		private static void Execute([CanBeNull] IT4File file, [CanBeNull] IT4TemplateExecutionManager manager)
+		private static void Execute([NotNull] IT4File file, [NotNull] ISolution solution)
 		{
-			if (file == null || manager == null) return;
+			var manager = solution.GetComponent<IT4TemplateExecutionManager>();
 			if (manager.IsExecutionRunning(file)) return;
 			manager.ExecuteSilently(file);
+		}
+
+		private static void Preprocess([NotNull] IT4File file, [NotNull] ISolution solution)
+		{
+			var targetFileManager = solution.GetComponent<IT4TargetFileManager>();
+			string message = new T4CSharpCodeGenerator(file, solution).Generate().RawText;
+			solution.Locks.ExecuteOrQueueEx(solution.GetLifetime(), "T4 template preprocessing", () =>
+			{
+				using (WriteLockCookie.Create())
+				{
+					targetFileManager.SavePreprocessResults(file, message);
+				}
+			});
 		}
 	}
 }
