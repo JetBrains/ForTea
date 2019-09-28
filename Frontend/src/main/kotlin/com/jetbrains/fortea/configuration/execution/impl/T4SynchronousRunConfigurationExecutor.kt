cfa62cce6209412e0b455b3da187c958eb173859
Kirill Glazyrin <kirillgla@gmail.com>
2019-09-29 00:00:23 +0300

    Make synchronous executor actually synchronous

--- Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/execution/impl/T4SynchronousRunConfigurationExecutor.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/execution/impl/T4SynchronousRunConfigurationExecutor.kt
@@ -5,6 +5,8 @@ import com.intellij.execution.executors.DefaultRunExecutor
 import com.intellij.execution.process.NopProcessHandler
 import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.openapi.project.Project
+import com.intellij.util.concurrency.Semaphore
+import com.jetbrains.rdclient.util.idea.pumpMessages
 import com.jetbrains.rider.projectView.ProjectModelViewHost
 
 class T4SynchronousRunConfigurationExecutor(
@@ -21,9 +23,26 @@ class T4SynchronousRunConfigurationExecutor(
       return
     }
 
+    val finished = Semaphore()
+    finished.down()
+
     val environment = builder.contentToReuse(null).dataContext(null).activeTarget().build()
-    ProgramRunnerUtil.executeConfiguration(environment, true, true)
-    val listener = project.messageBus.syncPublisher(ExecutionManager.EXECUTION_TOPIC)
-    listener.processStarted(executor.id, environment, NopProcessHandler())
+    ProgramRunnerUtil.executeConfigurationAsync(
+      environment,
+      true,
+      true
+    ) {
+      val listener = project.messageBus.syncPublisher(ExecutionManager.EXECUTION_TOPIC)
+      listener.processStarted(executor.id, environment, NopProcessHandler())
+      finished.up()
+    }
+    waitPumping(finished)
+  }
+
+  private tailrec fun waitPumping(finished: Semaphore) {
+    val acquired = finished.waitFor(10)
+    if (acquired) return
+    pumpMessages(10)
+    waitPumping(finished)
   }
 }
