fc703f466fb2f7683ee1cca2c90a8a5c86870a0f
Kirill Glazyrin <kirillgla@gmail.com>
2019-10-07 19:57:55 +0300

    Implement committing in two stages

---
+++ Backend/ForTea.Core/Psi/Invalidation/T4CommitStage.cs
@@ -0,0 +1,8 @@
+namespace GammaJul.ForTea.Core.Psi.Invalidation
+{
+	public enum T4CommitStage
+	{
+		UserChangeApplication,
+		DependencyInvalidation
+	}
+}
--- Backend/ForTea.Core/Psi/Invalidation/T4FileDependencyInvalidator.cs
+++ Backend/ForTea.Core/Psi/Invalidation/T4FileDependencyInvalidator.cs
@@ -11,13 +11,16 @@ namespace GammaJul.ForTea.Core.Psi.Invalidation
 	internal sealed class T4FileDependencyInvalidator
 	{
 		[NotNull, ItemNotNull]
-		public HashSet<FileSystemPath> CommittedFilePaths { get; } = new HashSet<FileSystemPath>();
+		private HashSet<FileSystemPath> CommittedFilePaths { get; } = new HashSet<FileSystemPath>();
 
 		[NotNull]
 		private T4FileDependencyManager FileDependencyManager { get; }
 
 		[NotNull]
 		private IPsiServices PsiServices { get; }
+
+		[NotNull]
+		private ILogger Logger { get; } = JetBrains.Util.Logging.Logger.GetLogger<T4FileDependencyInvalidator>();
 
 		public T4FileDependencyInvalidator(
 			[NotNull] T4FileDependencyManager fileDependencyManager,
@@ -32,20 +35,22 @@ namespace GammaJul.ForTea.Core.Psi.Invalidation
 
 		public void CommitNeededDocuments()
 		{
-			if (CommittedFilePaths.Count == 0) return;
+			int committedFilesCount = CommittedFilePaths.Count;
+			Logger.Verbose("{0} T4 files were committed during the current commit", committedFilesCount);
+			if (committedFilesCount == 0) return;
+
 			bool markedAsDirty;
 
-			// Mark includers file as dirty if their included files have changed.
 			using (WriteLockCookie.Create())
 			{
 				var includers = CommittedFilePaths
-					.SelectMany(committedFilePath => FileDependencyManager.GetIncluders(committedFilePath))
-					.Where(includer => !CommittedFilePaths.Contains(includer))
-					.Distinct() // Avoid unnecessary work
+					.SelectMany(FileDependencyManager.Graph.FindIndirectIncludesTransitiveClosure)
+					.Distinct()
 					.SelectMany(includer => PsiServices.Solution.FindProjectItemsByLocation(includer))
 					.OfType<IProjectFile>()
 					.AsList();
 				markedAsDirty = includers.Any();
+				Logger.Verbose("Marked {0} files as dirty because their dependencies changed", includers.Count);
 				foreach (var includer in includers)
 				{
 					PsiServices.MarkAsDirty(includer);
