1a845762c89fa82ecabb1f07f3764283fe4657cc
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-10-14 17:47:15 +0300

    Use correct resolve context when executing a template that used to be preprocessed

--- Backend/ForTea.RiderPlugin/ProtocolAware/Action/T4ExecuteTemplateActionBase.cs
+++ Backend/ForTea.RiderPlugin/ProtocolAware/Action/T4ExecuteTemplateActionBase.cs
@@ -1,19 +1,21 @@
+using GammaJul.ForTea.Core.TemplateProcessing.Services;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
 using JetBrains.Application.DataContext;
 using JetBrains.Application.UI.Actions;
+using JetBrains.Diagnostics;
 using JetBrains.ForTea.RiderPlugin.TemplateProcessing.Services;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Action
 {
-	public abstract class T4ProtocolAwareActionBase : T4FileBasedActionBase
+	public abstract class T4ExecuteTemplateActionBase : T4FileBasedActionBase
 	{
 		protected override bool DoUpdate(IPsiSourceFile file, ISolution solution) =>
 			!solution.GetComponent<IT4TemplateExecutionManager>().IsExecutionRunning(file);
 
-		public override void Execute(IDataContext context, DelegateExecute nextExecute)
+		public sealed override void Execute(IDataContext context, DelegateExecute nextExecute)
 		{
 			var solution = FindSolution(context);
 			if (solution == null) return;
@@ -22,13 +24,17 @@ namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Action
 			var executionManager = solution.GetComponent<IT4TemplateExecutionManager>();
 			var statistics = solution.GetComponent<Application.ActivityTrackingNew.UsageStatistics>();
 			var dataManager = solution.GetComponent<IT4ProjectModelTemplateDataManager>();
-			Execute(executionManager, file, statistics, dataManager);
+			var projectFile = file.GetSourceFile().NotNull().ToProjectFile().NotNull();
+			dataManager.SetTemplateKind(projectFile, T4TemplateKind.Executable);
+			statistics.TrackAction(ActionId);
+			// Template kind might have changed
+			solution.GetPsiServices().Files.CommitAllDocuments();
+			Execute(executionManager, file);
 		}
 
-		protected abstract void Execute(
-			[NotNull] IT4TemplateExecutionManager executionManager,
-			[NotNull] IT4File file,
-			[NotNull] Application.ActivityTrackingNew.UsageStatistics statistics,
-			[NotNull] IT4ProjectModelTemplateDataManager dataManager);
+		protected abstract void Execute([NotNull] IT4TemplateExecutionManager executionManager, [NotNull] IT4File file);
+
+		[NotNull]
+		protected abstract string ActionId { get; }
 	}
 }
