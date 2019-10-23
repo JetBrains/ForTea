7956d3ad2a7809f06684e262df1df52f76a86a14
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-10-23 20:10:50 +0300

    Make T4 file valid during secondary PSI construction

--- Backend/ForTea.Core/Tree/Impl/T4FileBase.cs
+++ Backend/ForTea.Core/Tree/Impl/T4FileBase.cs
@@ -6,6 +6,28 @@ namespace GammaJul.ForTea.Core.Tree.Impl
 {
 	public abstract class T4FileBase : FileElementBase, IT4TreeNode
 	{
+		/// The problem is, I need to access document ranges during construction of secondary PSI
+		/// and some other points in time when the file is considered invalid
+		/// because it has not registered in some caches yet.
+		/// There is an assert somewhere very deep in the platform that checks file validity
+		/// before converting tree ranges to document ranges.
+		/// <summary>
+		/// The most plausible correctness condition that still allows to work with document ranges
+		/// </summary>
+		public override bool IsValid()
+		{
+			// The part borrowed from FileElementBase because it needs to be overridden
+			if (parent == null) return true;
+			// The part borrowed from TreeElement because it cannot be accessed directly
+			TreeElement tmp = parent;
+			while (tmp.parent != null)
+			{
+				tmp = tmp.parent;
+			}
+
+			return tmp.IsValid();
+		}
+
 		public override PsiLanguageType Language => T4Language.Instance;
 		public abstract void Accept(TreeNodeVisitor visitor);
 		public abstract void Accept<TContext>(TreeNodeVisitor<TContext> visitor, TContext context);
