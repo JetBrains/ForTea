2b95ca6d001029e86932c3c7642198e67822393c
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-10-26 14:49:34 +0300

    [Refactor] Add another boilerplate wrapper

--- Backend/ForTea.RiderPlugin/ProtocolAware/IT4ModelInteractionHelper.cs
+++ Backend/ForTea.RiderPlugin/ProtocolAware/IT4ModelInteractionHelper.cs
@@ -2,13 +2,21 @@ using System;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
 using JetBrains.Core;
+using JetBrains.ReSharper.Psi;
 using JetBrains.Rider.Model;
 
 namespace JetBrains.ForTea.RiderPlugin.ProtocolAware
 {
 	public interface IT4ModelInteractionHelper
 	{
+		[NotNull]
 		Func<T4FileLocation, T> Wrap<T>([NotNull] Func<IT4File, T> wrappee, [NotNull] T defaultValue) where T : class;
+
+		[NotNull]
+		Func<T4FileLocation, T> Wrap<T>([NotNull] Func<IPsiSourceFile, T> wrappee, [NotNull] T defaultValue)
+			where T : class;
+
+		[NotNull]
 		Func<T4FileLocation, Unit> Wrap([NotNull] Action<IT4File> wrappee);
 	}
 }
