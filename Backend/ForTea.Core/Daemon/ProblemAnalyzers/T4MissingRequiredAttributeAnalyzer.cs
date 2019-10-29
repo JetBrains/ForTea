0472678d75fd8efd06ed886725f86049c587c7b3
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-10-29 13:56:48 +0300

    [Fix] Fix creepy error message

--- Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4MissingRequiredAttributeAnalyzer.cs
+++ Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4MissingRequiredAttributeAnalyzer.cs
@@ -48,7 +48,7 @@ namespace GammaJul.ForTea.Core.Daemon.ProblemAnalyzers
 			{
 				_ when infos.Count < 0 => "",
 				0 => throw new InvalidOperationException(),
-				1 => "Missing required attribute: {0}".FormatEx(infos.Single()),
+				1 => "Missing required attribute: {0}".FormatEx(infos.Single().Name),
 				_ => infos
 					.Take(infos.Count - 1)
 					.Aggregate(
