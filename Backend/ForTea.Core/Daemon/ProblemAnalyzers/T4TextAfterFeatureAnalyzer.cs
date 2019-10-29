7b6a2c4ecc02c4e089b7c3a3db86eac47d8310b9
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-10-29 13:33:13 +0300

    [Refactor] Split T4 daemon stage into process analyzers

---
+++ Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4TextAfterFeatureAnalyzer.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using GammaJul.ForTea.Core.Daemon.Highlightings;
+using GammaJul.ForTea.Core.Tree;
+using JetBrains.Annotations;
+using JetBrains.Diagnostics;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace GammaJul.ForTea.Core.Daemon.ProblemAnalyzers
+{
+	[ElementProblemAnalyzer(typeof(IT4Token), HighlightingTypes = new[] {typeof(TextAfterFeatureError)})]
+	public sealed class T4TextAfterFeatureAnalyzer : ElementProblemAnalyzer<IT4Token>
+	{
+		protected override void Run(
+			IT4Token element,
+			ElementProblemAnalyzerData data,
+			IHighlightingConsumer consumer
+		)
+		{
+			if (!HasFeatureBlocks(element)) return;
+			if (HasFeatureBlocksAhead(element)) return;
+			if (IsInFeatureBlock(element)) return;
+			consumer.AddHighlighting(new TextAfterFeatureError(element));
+		}
+
+		private static bool HasFeatureBlocks([NotNull] IT4Token element) =>
+			element.GetParentOfType<IT4FileLikeNode>().NotNull().Blocks.IsEmpty;
+
+		private static bool IsInFeatureBlock([NotNull] IT4Token element) =>
+			element.GetParentOfType<IT4FeatureBlock>() != null;
+
+		private static bool HasFeatureBlocksAhead([NotNull] IT4Token element) =>
+			element.NextTokens().Any(token => token.GetParentOfType<IT4FeatureBlock>() != null);
+	}
+}
