83ac49812ac760b4403e3c94caf62663465ab8b5
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-11-07 23:19:42 +0300

    Add shell build script

---
+++ BuildScripts/build.sh
@@ -0,0 +1,34 @@
+﻿#!/usr/bin/env bash
+{
+  pushd ..
+  {
+    {
+      pushd Frontend
+      ./gradlew :prepare --console=plain
+    } ||
+    {
+      popd
+    }
+  } &&
+  {
+    {
+      pushd Backend
+      dotnet build ForTea.Backend.sln
+    } ||
+    {
+      popd
+    }
+  } &&
+  {
+    {
+      pushd Frontend
+      ./gradlew :buildPlugin --console=plain
+    } ||
+    {
+      popd
+    }
+  }
+} ||
+{
+  popd
+}
