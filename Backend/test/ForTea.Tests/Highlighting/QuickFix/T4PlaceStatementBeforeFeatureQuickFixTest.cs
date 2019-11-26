2a5c26cdbab33b8514994d603f28249d25f0de1f
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-11-26 19:55:40 +0300

    Add quick fix tests

---
+++ Backend/test/ForTea.Tests/Highlighting/QuickFix/T4PlaceStatementBeforeFeatureQuickFixTest.cs
@@ -0,0 +1,18 @@
+using GammaJul.ForTea.Core.Daemon.QuickFixes.Moving;
+using GammaJul.ForTea.Core.Psi.FileType;
+using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace JetBrains.ForTea.Tests.Highlighting.QuickFix
+{
+	[TestFixture]
+	[Category("T4")]
+	[TestFileExtension(T4FileExtensions.MainExtension)]
+	public sealed class T4PlaceStatementBeforeFeatureQuickFixTest :
+		QuickFixTestBase<T4PlaceStatementBeforeFeatureQuickFix>
+	{
+		protected override string RelativeTestDataPath => @"Highlighting\QuickFix\PlaceStatementBeforeFeature";
+		[Test] public void Test01() => DoNamedTest2();
+	}
+}
---
+++ Backend/test/ForTea.Tests/Highlighting/QuickFix/T4ReplaceWithFeatureBlockQuickFixTest.cs
@@ -0,0 +1,18 @@
+using GammaJul.ForTea.Core.Daemon.QuickFixes;
+using GammaJul.ForTea.Core.Psi.FileType;
+using JetBrains.ReSharper.FeaturesTestFramework.Intentions;
+using JetBrains.ReSharper.TestFramework;
+using NUnit.Framework;
+
+namespace JetBrains.ForTea.Tests.Highlighting.QuickFix
+{
+	[TestFixture]
+	[Category("T4")]
+	[TestFileExtension(T4FileExtensions.MainExtension)]
+	public sealed class T4ReplaceWithFeatureBlockQuickFixTest : QuickFixTestBase<T4ReplaceWithFeatureBlockQuickFix>
+	{
+		protected override string RelativeTestDataPath => @"Highlighting\QuickFix\ReplaceWithFeatureBlock";
+		[Test] public void Test01() => DoNamedTest2();
+		[Test] public void Test02() => DoNamedTest2();
+	}
+}
