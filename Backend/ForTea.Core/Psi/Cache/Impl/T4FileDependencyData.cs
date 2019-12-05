69eac0b3389d5f34693dde3564bb2bf8898471a0
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-12-05 10:24:51 +0300

    [Refactor] Use existing marshaller

--- Backend/ForTea.Core/Psi/Cache/Impl/T4FileDependencyData.cs
+++ Backend/ForTea.Core/Psi/Cache/Impl/T4FileDependencyData.cs
@@ -7,8 +7,8 @@ namespace GammaJul.ForTea.Core.Psi.Cache.Impl
 	public sealed class T4FileDependencyData
 	{
 		[NotNull, ItemNotNull]
-		public List<FileSystemPath> Paths { get; }
+		public IList<FileSystemPath> Paths { get; }
 
-		public T4FileDependencyData([NotNull, ItemNotNull] List<FileSystemPath> includes) => Paths = includes;
+		public T4FileDependencyData([NotNull, ItemNotNull] IList<FileSystemPath> includes) => Paths = includes;
 	}
 }
--- Backend/ForTea.Core/Psi/Cache/Impl/T4FileDependencyDataMarshaller.cs
+++ Backend/ForTea.Core/Psi/Cache/Impl/T4FileDependencyDataMarshaller.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.Serialization;
 using JetBrains.Util;
@@ -9,10 +8,10 @@ namespace GammaJul.ForTea.Core.Psi.Cache.Impl
 {
 	public sealed class T4FileDependencyDataMarshaller : IUnsafeMarshaller<T4FileDependencyData>
 	{
-		private IUnsafeMarshaller<IList<string>> StringListMarshaller { get; } =
-			UnsafeMarshallers.GetCollectionMarshaller<string, IList<string>>(
-				UnsafeMarshallers.UnicodeStringMarshaller,
-				size => new List<string>(size)
+		private IUnsafeMarshaller<IList<FileSystemPath>> PathListMarshaller { get; } =
+			UnsafeMarshallers.GetCollectionMarshaller<FileSystemPath, IList<FileSystemPath>>(
+				UnsafeMarshallers.FileSystemPathComparableMarshaller,
+				size => new List<FileSystemPath>(size)
 			);
 
 		private T4FileDependencyDataMarshaller()
@@ -23,13 +22,10 @@ namespace GammaJul.ForTea.Core.Psi.Cache.Impl
 		public static T4FileDependencyDataMarshaller Instance { get; } = new T4FileDependencyDataMarshaller();
 
 		public void Marshal([NotNull] UnsafeWriter writer, [NotNull] T4FileDependencyData value) =>
-			StringListMarshaller.Marshal(writer, value.Paths.Select(includee => includee.FullPath).AsList());
+			PathListMarshaller.Marshal(writer, value.Paths);
 
 		[NotNull]
 		public T4FileDependencyData Unmarshal([NotNull] UnsafeReader reader) =>
-			new T4FileDependencyData(StringListMarshaller
-				.Unmarshal(reader)
-				.Select(path => FileSystemPath.Parse(path))
-				.AsList());
+			new T4FileDependencyData(PathListMarshaller.Unmarshal(reader));
 	}
 }
