3ce37892eba0ee7b48d14aff9e1dfd317a330b22
Kirill Glazyrin <Kirill.Glazyrin@gmail.com>
2019-12-07 11:50:32 +0300

    Use custom names for colors in UI

--- Backend/ForTea.Core/Daemon/Attributes/T4HighlightingAttributeIdRegistrations.cs
+++ Backend/ForTea.Core/Daemon/Attributes/T4HighlightingAttributeIdRegistrations.cs
@@ -1,5 +1,5 @@
 using GammaJul.ForTea.Core.Daemon.Attributes;
-using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Feature.Services.Daemon.Attributes;
 using JetBrains.ReSharper.Feature.Services.Daemon.Attributes.Idea;
 using JetBrains.TextControl.DocumentMarkup;
 
@@ -7,29 +7,34 @@ using JetBrains.TextControl.DocumentMarkup;
 	T4HighlightingAttributeIds.MACRO,
 	EffectType = EffectType.TEXT,
 	GroupId = T4HighlightingAttributeGroup.ID,
-	FallbackAttributeId = HighlightingAttributeIds.KEYWORD
+	FallbackAttributeId = DefaultLanguageAttributeIds.KEYWORD,
+	RiderPresentableName = "Macro"
 )]
 [assembly: RegisterHighlighter(
 	T4HighlightingAttributeIds.ENVIRONMENT_VARIABLE,
 	EffectType = EffectType.TEXT,
 	GroupId = T4HighlightingAttributeGroup.ID,
-	FallbackAttributeId = HighlightingAttributeIds.NUMBER
+	FallbackAttributeId = DefaultLanguageAttributeIds.NUMBER,
+	RiderPresentableName = "Environment Variable"
 )]
 [assembly: RegisterHighlighter(
 	T4HighlightingAttributeIds.ATTRIBUTE_VALUE,
 	EffectType = EffectType.TEXT,
 	GroupId = T4HighlightingAttributeGroup.ID,
-	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_ATTRIBUTE_VALUE
+	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_ATTRIBUTE_VALUE,
+	RiderPresentableName = "Attribute Value"
 )]
 [assembly: RegisterHighlighter(
 	T4HighlightingAttributeIds.DIRECTIVE,
 	EffectType = EffectType.TEXT,
 	GroupId = T4HighlightingAttributeGroup.ID,
-	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_TAG
+	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_TAG,
+	RiderPresentableName = "Directive Name"
 )]
 [assembly: RegisterHighlighter(
 	T4HighlightingAttributeIds.DIRECTIVE_ATTRIBUTE,
 	EffectType = EffectType.TEXT,
 	GroupId = T4HighlightingAttributeGroup.ID,
-	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_ATTRIBUTE_NAME
+	FallbackAttributeId = IdeaXmlHighlighterColorsAttributeIds.XML_ATTRIBUTE_NAME,
+	RiderPresentableName = "Attribute Name"
 )]
