c2ca76b7e0d596adf2e866a356fb75d39e28a0a7
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-02-01 21:08:31 +0300

    Add another test

---
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4PreprocessedTemplateInSubfolderClassNameTest.kt
@@ -0,0 +1,11 @@
+package com.jetbrains.fortea.highlighting
+
+import org.testng.annotations.Test
+
+class T4PreprocessedTemplateInSubfolderClassNameTest  : T4HighlightingTestBase() {
+  override fun getSolutionDirectoryName() = "ProjectWithComplexPreprocessedT4"
+  override val testFilePath: String
+    get() = "${getSolutionDirectoryName()}/Folder/$fileName"
+
+  @Test fun testClassName() = doTestAll()
+}
