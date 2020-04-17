4e85c93ca0c7bdbde23b389b8e1b948eec2aeb47
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-04-17 22:54:42 +0300

    Make tests more complex

--- Backend/test/ForTea.Tests/Psi/T4RangeTranslatorTest.cs
+++ Backend/test/ForTea.Tests/Psi/T4RangeTranslatorTest.cs
@@ -1,4 +1,5 @@
 using GammaJul.ForTea.Core.Tree;
+using JetBrains.Annotations;
 using JetBrains.Diagnostics;
 using JetBrains.DocumentManagers;
 using JetBrains.DocumentModel;
@@ -6,6 +7,7 @@ using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.ReSharper.TestFramework;
+using JetBrains.Util;
 using NUnit.Framework;
 
 namespace JetBrains.ForTea.Tests.Psi
@@ -13,9 +15,21 @@ namespace JetBrains.ForTea.Tests.Psi
 	public sealed class T4RangeTranslatorTest : BaseTestWithSingleProject
 	{
 		[Test]
-		public void TestThatTwoTranslationsAreInverseFunctions()
-		{
-			var fileNames = new[] {"Includer.tt", "Include.ttinclude", "Include2.ttinclude"};
+		public void TestIncludeLine() =>
+			DoTest("Includer.tt", "Include.ttinclude", "Include2.ttinclude");
+
+		[Test]
+		public void TestBigTree() => DoTest(
+			"Template1.tt",
+			"Template2.tt",
+			"Include.ttinclude",
+			"Include2.ttinclude",
+			"Include3.ttinclude",
+			"Include4.ttinclude",
+			"Include5.ttinclude"
+		);
+
+		private void DoTest([NotNull] params string[] fileNames) =>
 			WithSingleProject(fileNames, (lifetime, solution, project) =>
 			{
 				using var cookie = ReadLockCookie.Create();
@@ -25,9 +39,10 @@ namespace JetBrains.ForTea.Tests.Psi
 					var psiFile = (IT4File) projectFile.GetPrimaryPsiFile().NotNull();
 					var translator = ((IT4FileLikeNode) psiFile).DocumentRangeTranslator;
 					var document = projectFile.GetDocument();
-					// Do not want to analyze edge cases here
-					for (int offset = 1; offset < document.GetTextLength() - 2; offset += 1)
+					for (int offset = 1; offset < document.GetTextLength() - 1; offset += 1)
 					{
+						// Do not want to analyze edge cases here
+						if (IsEdgeOfIncludeCase(document, offset)) continue;
 						var originalDocumentRange = new DocumentRange(new DocumentOffset(document, offset));
 						var treeRange = translator.Translate(originalDocumentRange);
 						var resultingRange = translator.Translate(treeRange);
@@ -35,6 +50,14 @@ namespace JetBrains.ForTea.Tests.Psi
 					}
 				}
 			});
+
+		private static bool IsEdgeOfIncludeCase([NotNull] IDocument document, int offset)
+		{
+			Assert.That(offset < document.GetTextLength());
+			const string template = ".ttinclude\" #>";
+			int start = offset - template.Length;
+			if (start < 0) return false;
+			return document.GetText(new TextRange(start, offset)).Equals(template);
 		}
 	}
 }
