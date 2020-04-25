d33b59a6538fa717ce4cb70b0166eab5c082336d
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-04-25 22:35:40 +0300

    Always rerun tests

--- Frontend/build.gradle.kts
+++ Frontend/build.gradle.kts
@@ -192,8 +192,7 @@ tasks {
       showStandardStreams = true
       exceptionFormat = TestExceptionFormat.FULL
     }
-    val rerunSuccessfulTests = false
-    outputs.upToDateWhen { !rerunSuccessfulTests }
+    outputs.upToDateWhen { false }
     ignoreFailures = true
   }
 
