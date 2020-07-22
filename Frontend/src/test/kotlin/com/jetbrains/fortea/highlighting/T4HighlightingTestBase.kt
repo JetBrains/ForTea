aa971a73bd33d80ae8acdd23641b1365110086e5
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-07-22 21:32:52 +0300

    Add local environment to allowed paths

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4HighlightingTestBase.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4HighlightingTestBase.kt
@@ -1,14 +1,17 @@
 package com.jetbrains.fortea.highlighting
 
 import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.impl.EditorImpl
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.jetbrains.rdclient.testFramework.waitForDaemon
 import com.jetbrains.rider.test.base.BaseTestWithMarkup
 import com.jetbrains.rider.test.base.PrepareTestEnvironment
+import org.testng.annotations.AfterSuite
 import org.testng.annotations.BeforeSuite
 
-abstract class T4HighlightingTestBase : BaseTestWithMarkup() {
+abstract class T4HighlightingTestBase : BaseTestWithMarkup(), Disposable {
   abstract override fun getSolutionDirectoryName(): String
   protected open val fileName get() = "Template.tt"
   protected open val goldFileName get() = "$fileName.gold"
@@ -38,7 +41,14 @@ abstract class T4HighlightingTestBase : BaseTestWithMarkup() {
   private fun doTestWithMarkupModel(testAction: EditorImpl.() -> Unit) =
     doTestWithMarkupModel(fileName, testFilePath, goldFileName, testAction)
 
-//  @BeforeSuite(dependsOnMethods = ["initApplication"])
-//  fun postInitApplication() =
-//    VfsRootAccess.allowRootAccess(project, PrepareTestEnvironment.dotnetCoreCliPath)
+  override fun dispose() = Unit
+
+  @BeforeSuite(dependsOnMethods = ["initApplication"])
+  fun postSetupSolution() {
+    VfsRootAccess.allowRootAccess(this, PrepareTestEnvironment.dotnetCoreCliPath)
+    VfsRootAccess.allowRootAccess(this, PrepareTestEnvironment.msbuildPath)
+  }
+
+  @AfterSuite
+  fun postCloseSolution() = Disposer.dispose(this)
 }
