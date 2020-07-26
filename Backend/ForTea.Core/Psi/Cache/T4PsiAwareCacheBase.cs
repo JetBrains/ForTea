90dc4f74b8caed9fb597a27be7f50d173a224224
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-07-27 00:33:14 +0300

    Advance cache version

--- Backend/ForTea.Core/Psi/Cache/T4PsiAwareCacheBase.cs
+++ Backend/ForTea.Core/Psi/Cache/T4PsiAwareCacheBase.cs
@@ -14,7 +14,7 @@ namespace GammaJul.ForTea.Core.Psi.Cache
 	public abstract class T4PsiAwareCacheBase<TRequest, TResponse> : SimpleICache<TResponse> where TRequest : class
 	{
 		[NotNull]
-		public override string Version => "21";
+		public override string Version => "22";
 
 		protected T4PsiAwareCacheBase(
 			Lifetime lifetime,
