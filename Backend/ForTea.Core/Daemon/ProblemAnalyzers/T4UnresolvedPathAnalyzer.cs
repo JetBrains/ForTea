cf348a5ac9fcc7d65bff69c3596f4e06c9891ac8
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-07-27 16:48:11 +0300

    Move an analyzer into core part

--- Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4UnresolvedPathAnalyzer.cs
+++ Backend/ForTea.Core/Daemon/ProblemAnalyzers/T4UnresolvedPathAnalyzer.cs
@@ -1,5 +1,4 @@
 using GammaJul.ForTea.Core.Daemon.Highlightings;
-using GammaJul.ForTea.Core.Daemon.ProblemAnalyzers;
 using GammaJul.ForTea.Core.Psi.Directives;
 using GammaJul.ForTea.Core.Psi.Directives.Attributes;
 using GammaJul.ForTea.Core.Psi.Resolve.Assemblies;
@@ -7,7 +6,7 @@ using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Feature.Services.Daemon;
 
-namespace JetBrains.ForTea.RiderPlugin.Daemon.ProblemAnalyzers
+namespace GammaJul.ForTea.Core.Daemon.ProblemAnalyzers
 {
 	[ElementProblemAnalyzer(typeof(IT4AssemblyDirective), HighlightingTypes =
 		new[] {typeof(UnresolvedAssemblyError)})]
