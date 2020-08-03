e4dc77e81c3ed97c7ed08bddd02e3d18f7966b58
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-03 18:08:46 +0300

    Remove vfs root access hack

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4HighlightingTestBase.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4HighlightingTestBase.kt
@@ -1,17 +1,11 @@
 package com.jetbrains.fortea.highlighting
 
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.impl.EditorImpl
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess
 import com.jetbrains.rdclient.testFramework.waitForDaemon
 import com.jetbrains.rider.test.base.BaseTestWithMarkup
-import com.jetbrains.rider.test.base.PrepareTestEnvironment
-import org.testng.annotations.AfterSuite
-import org.testng.annotations.BeforeSuite
 
-abstract class T4HighlightingTestBase : BaseTestWithMarkup(), Disposable {
+abstract class T4HighlightingTestBase : BaseTestWithMarkup() {
   abstract override fun getSolutionDirectoryName(): String
   protected open val fileName get() = "Template.tt"
   protected open val goldFileName get() = "$fileName.gold"
@@ -40,15 +34,4 @@ abstract class T4HighlightingTestBase : BaseTestWithMarkup(), Disposable {
 
   private fun doTestWithMarkupModel(testAction: EditorImpl.() -> Unit) =
     doTestWithMarkupModel(fileName, testFilePath, goldFileName, testAction)
-
-  override fun dispose() = Unit
-
-  @BeforeSuite(dependsOnMethods = ["initApplication"])
-  fun postSetupSolution() {
-    VfsRootAccess.allowRootAccess(this, PrepareTestEnvironment.dotnetCoreCliPath)
-    VfsRootAccess.allowRootAccess(this, PrepareTestEnvironment.msbuildPath)
-  }
-
-  @AfterSuite
-  fun postCloseSolution() = Disposer.dispose(this)
 }
