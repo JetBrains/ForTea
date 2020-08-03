440de587be913983b3d9148d08e02d35e6e280c4
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-03 17:14:15 +0300

    Fix a NPE

--- Backend/ForTea.Core/TemplateProcessing/T4CSharpCodeGenerationUtils.cs
+++ Backend/ForTea.Core/TemplateProcessing/T4CSharpCodeGenerationUtils.cs
@@ -68,7 +68,7 @@ namespace GammaJul.ForTea.Core.TemplateProcessing
 				.FirstOrDefault()
 				?.GetFirstAttribute(extension)
 				?.Value
-				.GetText();
+				?.GetText();
 
 			if (targetExtension == null) return null;
 
