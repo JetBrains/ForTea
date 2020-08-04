589367c966026199edadeee873e77cd39844114c
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-04 16:49:15 +0300

    Add template base class member presence tests

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4PreprocessedTemplateHighlightingTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4PreprocessedTemplateHighlightingTest.kt
@@ -6,4 +6,6 @@ class T4PreprocessedTemplateHighlightingTest : T4HighlightingTestBase() {
   override fun getSolutionDirectoryName() = "ProjectWithPreprocessedT4"
 
   @Test fun testPartials() = doTestErrors()
+  @Test fun `test that default base class has no TransformText`() = doTestErrors()
+  @Test fun `test that default base class does not implement IDisposable`() = doTestErrors()
 }
