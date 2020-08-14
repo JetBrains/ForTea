f77bf2022bb7878b4add2ca879e322528bac35ad
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-14 12:47:27 +0300

    Fix an exception

--- Backend/ForTea.ReSharperPlugin/Psi/Resolve/Assemblies/Impl/T4LightWeightAssemblyReferenceResolver.cs
+++ Backend/ForTea.ReSharperPlugin/Psi/Resolve/Assemblies/Impl/T4LightWeightAssemblyReferenceResolver.cs
@@ -1,9 +1,7 @@
 using GammaJul.ForTea.Core.Psi.Resolve.Assemblies.Impl;
 using GammaJul.ForTea.Core.Psi.Resolve.Macros.Impl;
 using JetBrains.Annotations;
-using JetBrains.Diagnostics;
 using JetBrains.ProjectModel;
-using JetBrains.ReSharper.Psi;
 using JetBrains.Util;
 
 namespace JetBrains.ForTea.ReSharperPlugin.Psi.Resolve.Assemblies.Impl
