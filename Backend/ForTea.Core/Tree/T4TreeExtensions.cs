e7aee5d700090592c55e047d3dc9900ba8d5394a
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-20 16:30:38 +0300

    Fix a typo

--- Backend/ForTea.Core/Tree/T4TreeExtensions.cs
+++ Backend/ForTea.Core/Tree/T4TreeExtensions.cs
@@ -247,11 +247,11 @@ namespace GammaJul.ForTea.Core.Tree
 			}
 		}
 
-		private static bool ContainsIncludeContext([NotNull] this IT4File file) =>
+		private static bool ContainsNoIncludeContext([NotNull] this IT4File file) =>
 			file.LogicalPsiSourceFile == file.PhysicalPsiSourceFile;
 
 		public static void AssertContainsNoIncludeContext([NotNull] this IT4File file) =>
-			Assertion.Assert(file.ContainsIncludeContext(), "PSI file should not contain any include context");
+			Assertion.Assert(file.ContainsNoIncludeContext(), "PSI file should not contain any include context");
 
 		/// <summary>
 		/// Some nodes in T4 tree constitute include context.
