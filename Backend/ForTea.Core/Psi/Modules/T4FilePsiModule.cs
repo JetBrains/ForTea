f5bf7c833614e3df6251e5af422801009e44d558
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-08-25 17:16:20 +0300

    Make the change provider private again

--- Backend/ForTea.Core/Psi/Modules/T4FilePsiModule.cs
+++ Backend/ForTea.Core/Psi/Modules/T4FilePsiModule.cs
@@ -54,7 +54,7 @@ namespace GammaJul.ForTea.Core.Psi.Modules
 		[NotNull]
 		private string PersistentId { get; }
 
-		public IChangeProvider ChangeProvider { get; }
+		private IChangeProvider ChangeProvider { get; }
 		private ISolution Solution { get; }
 		public IPsiSourceFile SourceFile { get; }
 		public string Name => Prefix + SourceFile.Name;
