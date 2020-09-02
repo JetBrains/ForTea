17772dabac26477cacd2be20d814fe8fe155fae8
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-09-02 14:27:57 +0300

    Add a test draft

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4PreprocessedFileWithSharedIncludeHighlightingTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4PreprocessedFileWithSharedIncludeHighlightingTest.kt
@@ -1,2 +1,15 @@
 package com.jetbrains.fortea.highlighting
 
+import org.testng.annotations.Ignore
+import org.testng.annotations.Test
+
+class T4PreprocessedFileWithSharedIncludeHighlightingTest : T4HighlightingTestBase() {
+  override fun getSolutionDirectoryName() = "ProjectWithPreprocessedT4IncludedWithPartial"
+  override val fileName = "Include.ttinclude"
+  override val testFilePath: String
+    get() = "Project/$fileName"
+
+  @Ignore("TODO: implement test")
+  @Test
+  fun `test swea in template included into multiple files`() = doTestErrors()
+}
