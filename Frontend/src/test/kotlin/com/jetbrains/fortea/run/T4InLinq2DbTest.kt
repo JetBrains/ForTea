b9661dd42bdbcf01781577af2aaa81834cc28086
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2020-12-28 13:39:24 +0300

    Remove solution logging

--- Frontend/src/test/kotlin/com/jetbrains/fortea/run/T4InLinq2DbTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/run/T4InLinq2DbTest.kt
@@ -1,7 +1,5 @@
 package com.jetbrains.fortea.run
 
-import com.intellij.openapi.application.PathManager
-import com.jetbrains.rider.test.framework.combine
 import org.testng.annotations.Test
 import java.time.Duration
 
@@ -10,14 +8,5 @@ class T4InLinq2DbTest : T4RunFileTestBase() {
   override val backendLoadedTimeout: Duration = Duration.ofMinutes(10)
 
   @Test
-  fun testDefaultLinq2DbTemplate() {
-    try {
-      doTest(".generated.cs")
-    }
-    finally {
-      val target = java.io.File(PathManager.getLogPath()).combine("testDefaultLinq2DbTemplate")
-      if (target.exists()) target.deleteRecursively()
-      java.io.File(project.basePath).copyRecursively(target)
-    }
-  }
+  fun testDefaultLinq2DbTemplate() = doTest(".generated.cs")
 }
