61f40ae5ec1b62daa3258c6174d99920196eb58c
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2021-01-02 20:14:25 +0300

    Use core toolset in T4TemplateInMultiTargetProjectHighlightingTest

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4TemplateInMultiTargetProjectHighlightingTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4TemplateInMultiTargetProjectHighlightingTest.kt
@@ -1,7 +1,11 @@
 package com.jetbrains.fortea.highlighting
 
+import com.jetbrains.rider.test.annotations.TestEnvironment
+import com.jetbrains.rider.test.enums.CoreVersion
+import com.jetbrains.rider.test.enums.ToolsetVersion
 import org.testng.annotations.Test
 
+@TestEnvironment(toolset = ToolsetVersion.TOOLSET_16_CORE, coreVersion = CoreVersion.DEFAULT)
 class T4TemplateInMultiTargetProjectHighlightingTest : T4HighlightingTestBase() {
   override fun getSolutionDirectoryName() = "ProjectWithMultipleTargetFrameworks"
   override val testFilePath get() = "Project/$fileName"
