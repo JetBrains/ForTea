925a92a5b29e7f28ea31c2a1e71029e28b91b39c
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2021-04-19 21:04:36 +0300

    Add another cloning parser correctness test

--- Backend/test/ForTea.Tests/Mock/T4MockIncludeParser.cs
+++ Backend/test/ForTea.Tests/Mock/T4MockIncludeParser.cs
@@ -1,12 +1,15 @@
 ﻿using GammaJul.ForTea.Core.Parsing.Parser.Impl;
 using GammaJul.ForTea.Core.Tree;
+using JetBrains.Annotations;
 using JetBrains.ReSharper.Psi.Tree;
 
 namespace JetBrains.ForTea.Tests.Mock
 {
 	public sealed class T4MockIncludeParser : IT4IncludeParser
-
 	{
-		public ITreeNode Parse(IT4IncludeDirective directive) => null;
+		[CanBeNull]
+		private ITreeNode Node { get; }
+		public T4MockIncludeParser([CanBeNull] ITreeNode node = null) => Node = node;
+		public ITreeNode Parse(IT4IncludeDirective directive) => Node;
 	}
 }
--- Backend/test/ForTea.Tests/Psi/T4CloningParserTest.cs
+++ Backend/test/ForTea.Tests/Psi/T4CloningParserTest.cs
@@ -1,7 +1,11 @@
 ﻿using System.IO;
+using System.Linq;
 using GammaJul.ForTea.Core.Parsing.Lexing;
 using GammaJul.ForTea.Core.Parsing.Parser;
+using GammaJul.ForTea.Core.Psi.Resolve.Macros.Impl;
 using GammaJul.ForTea.Core.Test;
+using GammaJul.ForTea.Core.Tree;
+using GammaJul.ForTea.Core.Tree.Impl;
 using JetBrains.Annotations;
 using JetBrains.Diagnostics;
 using JetBrains.ForTea.Tests.Mock;
@@ -16,14 +20,33 @@ namespace JetBrains.ForTea.Tests.Psi
 		[Test]
 		public void TestCorrectness()
 		{
-			const string text = "<#@ template language=\"C#\"#>\n<#@ output extension=\"cs\" #>\n";
+			const string text = "<#@ template language=\"C#\" #>\n<#@ output extension=\"cs\" #>\n";
 			var parser = T4ParserExposer.Create(text, new T4MockIncludeParser());
 			var file = parser.ParseFile();
 			file.NotNull("Could not build a PSI tree");
 			string mainPsi = PsiToString(file);
+
 			var clone = new T4CloningParser(new T4MockPsiFileProvider(file), null, T4DocumentLexerSelector.Instance).ParseFile();
 			string clonePsi = PsiToString(clone);
 			Assert.AreEqual(mainPsi, clonePsi, "Cloned PSI is different from original PSI");
+		}
+
+		[Test]
+		public void TestCorrectnessWithInclude()
+		{
+			const string includeText = "<#@ using namespace=\"System.Text\" #>\n<#+\n    public void Foo()\n    {\n    }\n#>";
+			const string includerText = "<#@ template language=\"C#\" #>\n<#@ output extension=\"cs\" #>\nSome random text<#@ include file=\"dummy.ttinclude\" #>More random text\nEven more random text";
+
+			var includePsi = T4ParserExposer.Create(includeText, new T4MockIncludeParser()).ParseFile();
+			var includerPsi = T4ParserExposer.Create(includerText, new T4MockIncludeParser(includePsi)).ParseFile();
+			string includerPsiText = PsiToString(includerPsi.NotNull());
+			InitializeResolvePaths((IT4File) includerPsi);
+
+			var psiProvider = new T4MockPsiFileProvider(includerPsi).NotNull();
+			var clonePsi = new T4CloningParser(psiProvider, null, T4DocumentLexerSelector.Instance).ParseFile();
+			string clonePsiText = PsiToString(clonePsi);
+
+			Assert.AreEqual(includerPsiText, clonePsiText, "Cloned PSI is different from original PSI");
 		}
 
 		[NotNull]
@@ -33,6 +56,14 @@ namespace JetBrains.ForTea.Tests.Psi
 			DebugUtil.DumpPsi(writer, file);
 			string result = writer.GetStringBuilder().ToString();
 			return result;
+		}
+
+		private static void InitializeResolvePaths([NotNull] IT4File file)
+		{
+			foreach (var directive in file.BlocksEnumerable.OfType<T4DirectiveWithPathBase>())
+			{
+				directive.InitializeResolvedPath(new T4ResolvedPath("dummy", null, null));
+			}
 		}
 	}
 }
