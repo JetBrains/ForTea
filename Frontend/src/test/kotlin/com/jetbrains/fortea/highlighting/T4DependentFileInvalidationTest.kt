01c5a43652af4e2abfada06e5373206b77b04704
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2021-06-17 12:08:11 +0300

    Fix compilation after platform API change

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4DependentFileInvalidationTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4DependentFileInvalidationTest.kt
@@ -7,7 +7,7 @@ import com.jetbrains.rdclient.daemon.components.FrontendMarkupHost
 import com.jetbrains.rdclient.daemon.util.severity
 import com.jetbrains.rdclient.testFramework.typeWithLatency
 import com.jetbrains.rdclient.testFramework.waitForDaemon
-import com.jetbrains.rider.daemon.util.annotateDocumentWithHighlighterTags
+import com.jetbrains.rdclient.daemon.util.annotateDocumentWithHighlighterTags
 import com.jetbrains.rider.test.base.EditorTestBase
 import com.jetbrains.rider.test.framework.executeWithGold
 import com.jetbrains.rider.test.scriptingApi.commitBackendPsiFiles
