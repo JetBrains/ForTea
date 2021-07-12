1fbb43980045d1bae8f71bf21cf0947152fb6177
denis.kuznetsov <denis.kuznetsov@jetbrains.com>
2021-07-12 20:04:36 +0300

    Prepare for git subs - part 2

--- Backend/RiderPlugin/test/src/TestEnvironment.cs
+++ Backend/RiderPlugin/test/src/TestEnvironment.cs
@@ -2,25 +2,32 @@ using System.Threading;
 using GammaJul.ForTea.Core;
 using JetBrains.ForTea.RiderPlugin;
 using JetBrains.TestFramework;
-using JetBrains.TestFramework.Application.Zones;
 using NUnit.Framework;
 
 [assembly: RequiresThread(ApartmentState.STA)]
 
 namespace JetBrains.ForTea.Tests
 {
-	[SetUpFixture]
-	public sealed class TestEnvironment : TestEnvironmentAssembly<IT4TestZone>
-	{
-		public override bool IsRunningTestsWithAsyncBehaviorProhibited
-		{
-			get { return true; }
-		}
 
+#if INDEPENDENT_BUILD
+	[SetUpFixture]
+	public sealed class TestEnvironment : T4ExtensionTestEnvironmentAssembly<IT4TestZone> // HACK
+	{
 #pragma warning disable 169
 		// These fields are here to force load assemblies
-		// private IT4Environment magic1; // ForTea.Core
-		// private T4RiderEnvironment magic2; // ForTea.RiderPlugin
+		private IT4Environment magic1; // ForTea.Core
+		private T4RiderEnvironment magic2; // ForTea.RiderPlugin
 #pragma warning restore 169
 	}
+#else
+	[SetUpFixture]
+	public sealed class TestEnvironment : ExtensionTestEnvironmentAssembly<IT4TestZone>
+	{
+		public override bool IsRunningTestsWithAsyncBehaviorProhibited => true;
+
+	}
+#endif
+	
+
+
 }
