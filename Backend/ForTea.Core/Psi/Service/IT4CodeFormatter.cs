b172ff102c4e137caa092bdc007c8e87bd9d0610
Kirill Glazyrin <kirill.glazyrin@jetbrains.com>
2021-08-02 17:11:10 +0300

    Fix the T4 aspect of RIDER-65550

---
+++ Backend/ForTea.Core/Psi/Service/IT4CodeFormatter.cs
@@ -0,0 +1,8 @@
+using JetBrains.ReSharper.Psi.CodeStyle;
+
+namespace GammaJul.ForTea.Core.Psi.Service
+{
+	public interface IT4CodeFormatter : ICodeFormatter
+	{
+	}
+}
---
+++ Backend/ForTea.Core/Psi/Service/T4CodeFormattingSettingsKey.cs
@@ -0,0 +1,13 @@
+using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Psi.CodeStyle;
+using JetBrains.ReSharper.Psi.EditorConfig;
+using JetBrains.ReSharper.Psi.Format;
+
+namespace GammaJul.ForTea.Core.Psi.Service
+{
+	[SettingsKey(typeof(CodeFormattingSettingsKey), "Code formatting in T4")]
+	[EditorConfigKey("T4")]
+	public sealed class T4CodeFormattingSettingsKey : FormatSettingsKeyBase
+	{
+	}
+}
