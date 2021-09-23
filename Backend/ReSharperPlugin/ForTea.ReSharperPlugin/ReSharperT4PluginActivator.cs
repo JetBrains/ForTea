57e5d4b2fb88cfe08e05cf750f5a22f3b63c2674
baltic <baltic@intellij.net>
2021-09-23 08:32:43 +0300

    Basic zoning for the T4 plugin. Add a feature zone for plugin stuff and activate it by default in supported envirnoments. Fix validation errors. Possible option: no special zone for the plugin, just mark with all applicable zones and do not activate anything. Won't be able to turn off as a feature in R#, and less structural in markers, but might be easier to maintain.

---
+++ Backend/ReSharperPlugin/ForTea.ReSharperPlugin/ReSharperT4PluginActivator.cs
@@ -0,0 +1,16 @@
+using GammaJul.ForTea.Core;
+
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.Application.Environment;
+using JetBrains.Platform.VisualStudio.SinceVs10.Shell.Zones;
+
+#pragma warning disable CheckNamespace
+
+namespace JetBrains.ForTea.ReSharperPluginActivation
+{
+	[ZoneActivator]
+	[ZoneMarker(typeof(ISinceVs10FrontEnvZone))]
+	public class ReSharperT4PluginActivator : IActivate<IT4Zone>
+	{
+	}
+}
