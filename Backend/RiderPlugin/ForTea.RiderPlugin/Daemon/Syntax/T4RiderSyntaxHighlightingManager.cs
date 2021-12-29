a0af671b5dd0fc5d5f501e5a96293c01d54556f6
Vladimir Krasnotsvetov <vladimir.krasnotsvetov@jetbrains.com>
2021-12-29 14:17:34 +0300

    share syntax highlighting between fleet and rider

--- Backend/RiderPlugin/ForTea.RiderPlugin/Daemon/Syntax/T4RiderSyntaxHighlightingManager.cs
+++ Backend/RiderPlugin/ForTea.RiderPlugin/Daemon/Syntax/T4RiderSyntaxHighlightingManager.cs
@@ -1,12 +1,12 @@
 using GammaJul.ForTea.Core.Psi;
-using JetBrains.IdeaInterop.Backend.Common.Features.Daemon;
+using JetBrains.ReSharper.Daemon.Syntax;
 using JetBrains.ReSharper.Daemon.SyntaxHighlighting;
 using JetBrains.ReSharper.Psi;
 
 namespace JetBrains.ForTea.RiderPlugin.Daemon.Syntax
 {
 	[Language(typeof(T4Language))]
-	public class T4RiderSyntaxHighlightingManager : RiderSyntaxHighlightingManager
+	public class T4SyntaxHighlightingManager : SyntaxHighlightingManager
 	{
 		public override SyntaxHighlightingProcessor CreateProcessor() => new T4SyntaxHighlightingProcessor();
 	}
