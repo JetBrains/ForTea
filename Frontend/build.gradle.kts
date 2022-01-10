6c9c9f1ad9494c0c3bc290f5c33f84c11d2a80fd
Mikhail Filippov <mikhail@filippov.me>
2022-01-10 17:03:10 +0300

    Try to fix T4.

--- Frontend/build.gradle.kts
+++ Frontend/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
   id("org.jetbrains.grammarkit") version "2021.1.3"
   id("me.filippov.gradle.jvm.wrapper") version "0.9.3"
   id ("com.jetbrains.rdgen") version "2021.3.4"
-  kotlin("jvm") version "1.4.10"
+  kotlin("jvm") version "1.6.10"
 }
 
 apply {
