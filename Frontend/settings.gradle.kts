5edeacd45671e53c90f9df41f5d07252b455c60a
Mikhail Filippov <mikhail@filippov.me>
2022-05-21 21:30:33 +0300

    Bump intellij plugin version for ForTea

--- Frontend/settings.gradle.kts
+++ Frontend/settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "ForTea"
 pluginManagement {
   repositories {
     gradlePluginPortal()
+    maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
     // This is for snapshot version of 'org.jetbrains.intellij' plugin
     maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
   }
