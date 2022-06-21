d2a386435e2a1cedeba05b427f70a868171ff728
Ivan Migalev <ivan.migalev@jetbrains.com>
2022-06-21 17:59:29 +0700

    Rider plugins: add mavenLocal for easier local rd-gen usage

--- Frontend/settings.gradle.kts
+++ Frontend/settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
     maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
     // This is for snapshot version of 'org.jetbrains.intellij' plugin
     maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
+    mavenLocal()
   }
   resolutionStrategy {
     eachPlugin {
