d7b005cb701db27d13b8aa3bc846ba614db83d0d
Evgeniy.Stepanov <Evgeniy.Stepanov@JetBrains.com>
2022-08-15 21:57:49 +0200

    Update jvm target to 17 according to platform changes

--- Frontend/build.gradle.kts
+++ Frontend/build.gradle.kts
@@ -190,7 +190,7 @@ tasks {
   }
 
   withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "17"
     this.kotlinOptions.freeCompilerArgs = listOf("-Xjvm-default=enable")
   }
 
