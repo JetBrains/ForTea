998dc4b015a23f18a5991193174190039df52b59
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2022-08-27 01:05:51 -0600

    Update quickdocs in T4 to the new API

---
+++ Backend/Core/ForTea.Core/Daemon/Quickdocs/T4QuickDocPresenterBase.cs
@@ -0,0 +1,46 @@
+using JetBrains.Annotations;
+using JetBrains.ReSharper.Feature.Services.QuickDoc;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.UI.RichText;
+
+namespace GammaJul.ForTea.Core.Daemon.Quickdocs
+{
+	public abstract class T4QuickDocPresenterBase<TExpandable> : IQuickDocPresenter where TExpandable : ITreeNode
+	{
+		[NotNull] private TExpandable Expandable { get; }
+		[NotNull] protected abstract string ExpandableName { get; }
+
+		protected T4QuickDocPresenterBase([NotNull] TExpandable expandable)
+		{
+			Expandable = expandable;
+		}
+
+		public QuickDocTitleAndText GetHtml(PsiLanguageType presentationLanguage)
+		{
+			var expanded = Expand(Expandable) ?? "(unresolved)";
+			return new QuickDocTitleAndText(new RichText($"({ExpandableName}) {expanded}"), "T4 Quickdocs");
+		}
+
+		[CanBeNull]
+		protected abstract string Expand([NotNull] TExpandable expandable);
+
+		public string GetId()
+		{
+			return "T4 Quickdoc Placeholder Id";
+		}
+
+		public IQuickDocPresenter Resolve(string id)
+		{
+			return null;
+		}
+
+		public void OpenInEditor(string navigationId = "")
+		{
+		}
+
+		public void ReadMore(string navigationId = "")
+		{
+		}
+	}
+}
---
+++ Backend/Core/ForTea.Core/Daemon/Quickdocs/T4QuickDocProvider.cs
@@ -0,0 +1,53 @@
+using System;
+using GammaJul.ForTea.Core.Psi;
+using GammaJul.ForTea.Core.Psi.Resolve.Macros;
+using GammaJul.ForTea.Core.Tree;
+using JetBrains.Application.DataContext;
+using JetBrains.Diagnostics;
+using JetBrains.DocumentModel.DataContext;
+using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.DataContext;
+using JetBrains.ReSharper.Feature.Services.QuickDoc;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.DataContext;
+using JetBrains.ReSharper.Psi.Files;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace GammaJul.ForTea.Core.Daemon.Quickdocs
+{
+	public class T4QuickDocProvider : IQuickDocProvider
+	{
+		public bool CanNavigate(IDataContext context)
+		{
+			var token = TryFindToken(context);
+			return token != null
+			       && (token.GetParentOfType<IT4Macro>() != null
+			           || token.GetParentOfType<IT4EnvironmentVariable>() != null);
+		}
+
+		private static ITreeNode TryFindToken(IDataContext context)
+		{
+			var editorContext = context.GetData(DocumentModelDataConstants.EDITOR_CONTEXT);
+			if (editorContext == null) return null;
+			var sourceFile = context.GetData(PsiDataConstants.SOURCE_FILE);
+			var file = sourceFile?.GetPsiFile(T4Language.Instance, editorContext.CaretOffset);
+			return file?.FindTokenAt(editorContext.CaretOffset);
+		}
+
+		public void Resolve(IDataContext context, Action<IQuickDocPresenter, PsiLanguageType> resolved)
+		{
+			var token = TryFindToken(context).NotNull();
+			var macro = token.GetParentOfType<IT4Macro>();
+			if (macro != null)
+			{
+				var solution = context.GetData(ProjectModelDataConstants.SOLUTION).NotNull();
+				var resolver = solution.GetComponent<IT4MacroResolver>();
+				resolved(new T4MacroQuickDocPresenter(macro, resolver), T4Language.Instance);
+				return;
+			}
+
+			var environmentVariable = token.GetParentOfType<IT4EnvironmentVariable>().NotNull();
+			resolved(new T4EnvironmentQuickDocPresenter(environmentVariable), T4Language.Instance);
+		}
+	}
+}
