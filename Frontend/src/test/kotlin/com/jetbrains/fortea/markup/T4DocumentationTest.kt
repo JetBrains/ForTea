92fb8d280ef8a5202deb0a43632cc8f0e990a81a
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2022-10-02 11:22:01 -0700

    Update tests

---
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/markup/T4DocumentationTest.kt
@@ -0,0 +1,31 @@
+package com.jetbrains.fortea.markup
+
+import com.jetbrains.fortea.daemon.T4HighlightingAttributeIds
+import com.jetbrains.rdclient.testFramework.waitForDaemon
+import com.jetbrains.rider.test.base.DocumentationTestBase
+import com.jetbrains.rider.test.scriptingApi.`is`
+import org.testng.annotations.Ignore
+import org.testng.annotations.Test
+
+@Ignore
+class T4DocumentationTest : DocumentationTestBase() {
+  override fun getSolutionDirectoryName() = "ProjectWithT4"
+
+  @Test
+  fun testMacroToolTip() {
+    doTestWithMarkupModel("Template.tt", "Template.tt") {
+      waitForDaemon()
+      dumpHighlightersTree({ it `is` T4HighlightingAttributeIds.MACRO }, {
+        val doc = generateBackendHoverDoc(it.startOffset)
+        assert(doc.endsWith('/') || doc.endsWith('\\'))
+        val docWithoutTrailingSlash = doc.substring(0, doc.lastIndex)
+        ".../" + docWithoutTrailingSlash.substring(
+          maxOf(
+            docWithoutTrailingSlash.lastIndexOf('/') + 1,
+            docWithoutTrailingSlash.lastIndexOf('\\') + 1
+          )
+        )
+      })
+    }
+  }
+}
