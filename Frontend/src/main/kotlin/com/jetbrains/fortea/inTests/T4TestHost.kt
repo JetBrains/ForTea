6fba241ad071558e8e3dff5b04be9cb4b00f5188
Mikhail Filippov <mikhail@filippov.me>
2022-10-13 16:10:15 +0400

    Fix T4 tests

--- Frontend/src/main/kotlin/com/jetbrains/fortea/inTests/T4TestHost.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/inTests/T4TestHost.kt
@@ -5,26 +5,25 @@ import com.jetbrains.rdclient.protocol.ProtocolComponent
 import com.jetbrains.rdclient.util.idea.callSynchronously
 import com.jetbrains.rider.inTests.InTestsConstants
 import com.jetbrains.fortea.model.T4FileLocation
-import com.jetbrains.fortea.model.T4TestModel
+import com.jetbrains.fortea.model.t4TestModel
 import com.jetbrains.rider.protocol.ProtocolManager
 import org.apache.commons.logging.LogFactory
 
 class T4TestHost(protocolHost: IProtocolHost) : ProtocolComponent(protocolHost) {
-  private val model = T4TestModel.create(lifetime, protocol)
   private val frameworkLogger = LogFactory.getLog(InTestsConstants.frameworkLoggerName)!!
 
   // TODO: fun waitForIndirectFileInvalidation() {...}
 
   fun preprocessFile(t4FileLocation: T4FileLocation) {
     if (ProtocolManager.isResharperBackendDisabled()) throw IllegalStateException("ReSharper backend is disabled")
-    model.preprocessFile.callSynchronously(t4FileLocation, protocol, lifetime)
+    protocol.t4TestModel.preprocessFile.callSynchronously(t4FileLocation, protocol, lifetime)
     frameworkLogger.info("Complete template preprocessing")
   }
 
   fun waitForIndirectInvalidation() {
     if (ProtocolManager.isResharperBackendDisabled()) throw IllegalStateException("ReSharper backend is disabled")
     frameworkLogger.info("Starting waitForIndirectInvalidation")
-    model.waitForIndirectInvalidation.callSynchronously(Unit, protocol, lifetime)
+    protocol.t4TestModel.waitForIndirectInvalidation.callSynchronously(Unit, protocol, lifetime)
     frameworkLogger.info("Complete waitForIndirectInvalidation")
   }
 
