6de560a826f21602247a5e118cb22c7a094a3272
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2022-11-30 06:43:48 -0800

    Fix RIDER-82037, for real this time

--- Frontend/src/main/kotlin/com/jetbrains/fortea/actions/T4SolutionExplorerCustomization.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/actions/T4SolutionExplorerCustomization.kt
@@ -4,12 +4,21 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope
 import com.jetbrains.fortea.language.T4FileType
+import com.jetbrains.rider.ideaInterop.find.scopes.RiderSingleProjectModelEntityScope
 import com.jetbrains.rider.projectView.views.solutionExplorer.SolutionExplorerCustomization
+import com.jetbrains.rider.projectView.workspace.containingProjectEntity
+import com.jetbrains.rider.projectView.workspace.getProjectModelEntity
 
 class T4SolutionExplorerCustomization(project: Project): SolutionExplorerCustomization(project) {
   override fun getNonImportantActionsForAddGroup(e: AnActionEvent) =
-    if (DumbService.isDumb(project) || !FileTypeIndex.containsFileOfType(T4FileType, GlobalSearchScope.projectScope(project))) listOf("NewT4File")
-    else super.getNonImportantActionsForAddGroup(e)
+    if (shouldShowT4Template(e)) super.getNonImportantActionsForAddGroup(e)
+    else listOf("NewT4File")
+
+  private fun shouldShowT4Template(e: AnActionEvent): Boolean {
+    if (DumbService.isDumb(project)) return false
+    val entity = e.dataContext.getProjectModelEntity(false)?.containingProjectEntity() ?: return false
+    val scope = RiderSingleProjectModelEntityScope(project, entity, true)
+    return FileTypeIndex.containsFileOfType(T4FileType, scope)
+  }
 }
