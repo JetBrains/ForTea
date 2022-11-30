9a3b3461337343b0ce6751fe7bedef9b5b573d75
Kirill Glazyrin <Kirill.Glazyrin@jetbrains.com>
2022-11-30 06:42:07 -0800

    Change version numbers to 231

--- Frontend/build.gradle.kts
+++ Frontend/build.gradle.kts
@@ -40,7 +40,7 @@ dependencies {
   testImplementation(kotlin("test"))
 }
 
-val baseVersion = "2022.3"
+val baseVersion = "2023.1"
 val buildCounter = buildNumber ?: "9999"
 version = "$baseVersion.$buildCounter"
 
