c85d7e7818185933bf77d347722a1561ebf290c2
Ilia Kirianovskii <Ilia.Kirianovskii@jetbrains.com>
2023-05-10 07:29:31 +0200

    Platform: Emit application .dll.config files w/ binding redirects for tests (NP-1605)
    Also adds BindingRedirects.GenerateConfigurationFileBytes() (in JetBrains.Application.BuildScript.BindingRedirects) to generate configuration file bytes with bindingRedirect,
    and GenerateDllConfig.Run()/RequestAppConfigInstall.Run() to automatically generate/install them from plugins.

---
+++ Backend/RiderPlugin/BuildScript/RequestAppConfigInstall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Application.BuildScript.Install;
+using JetBrains.Application.BuildScript.Solution;
+using JetBrains.Build;
+
+namespace JetBrains.Plugins.ForTea.Backend.RiderPlugin.BuildScript;
+
+public class RequestAppConfigInstall
+{
+  [BuildStep]
+  public static InstallAppConfig[] Run(AllAssembliesOnEverything allass)
+  {
+    return allass.FindSubplatformByClass<RequestAppConfigInstall>() is SubplatformOnSources subplatform ? new[] {new InstallAppConfig(subplatform.Name, "test/ForTea.Tests.csproj")} : Array.Empty<InstallAppConfig>();
+  }
+}
