34dc350b7e9b431d650fb589cc6a170bbefc011f
Ilya.Usov <Ilya.Usov@jetbrains.com>
2023-10-03 16:43:17 +0200

    Move FrontendDocumentHostListener to app level
    For most implementations, use overloading with the protocol

--- Frontend/src/main/kotlin/com/jetbrains/fortea/highlighting/T4SyntaxHighlightingHost.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/highlighting/T4SyntaxHighlightingHost.kt
@@ -7,36 +7,35 @@ import com.intellij.openapi.util.Key
 import com.intellij.openapi.util.getOrCreateUserData
 import com.jetbrains.fortea.model.T4RdDocumentModel
 import com.jetbrains.fortea.model.t4RdDocumentModel
+import com.jetbrains.rd.framework.IProtocol
 import com.jetbrains.rd.ide.model.RdDocumentId
 import com.jetbrains.rd.ide.model.RdDocumentModel
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.put
 import com.jetbrains.rdclient.document.FrontendDocumentHostListener
-import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
+import com.jetbrains.rider.protocol.toProject
 
-// TODO: Remove the service at all, keep only DocumentListener
-class T4SyntaxHighlightingHost(project: Project) : LifetimedProjectComponent(project) {
+class T4SyntaxHighlightingHost {
   companion object {
-    fun getInstance(project: Project): T4SyntaxHighlightingHost = project.service()
 
     private val T4_DOCUMENT_EDITABLE_ENTRY_KEY = Key<MutableMap<Document, T4RdDocumentModel>>("T4_DOCUMENT_EDITABLE_ENTRY_KEY")
 
     fun Document.getT4RdDocumentModel(project: Project) =
       project.getUserData(T4_DOCUMENT_EDITABLE_ENTRY_KEY)?.get(this)
 
-    private fun putT4RdDocumentModel(lifetime: Lifetime, project: Project, document: Document, model: T4RdDocumentModel) {
-      val map = project.getOrCreateUserData(T4_DOCUMENT_EDITABLE_ENTRY_KEY) { mutableMapOf() }
+    private fun putT4RdDocumentModel(lifetime: Lifetime, protocol: IProtocol, document: Document, model: T4RdDocumentModel) {
+      val map = protocol.toProject.getOrCreateUserData(T4_DOCUMENT_EDITABLE_ENTRY_KEY) { mutableMapOf() }
       map.put(lifetime, document, model)
     }
   }
 
   class DocumentListener : FrontendDocumentHostListener {
     override fun documentBound(lifetime: Lifetime,
-                               project: Project,
+                               protocol: IProtocol,
                                documentId: RdDocumentId,
                                documentModel: RdDocumentModel,
                                document: Document) {
-      putT4RdDocumentModel(lifetime, project, document, documentModel.t4RdDocumentModel)
+      putT4RdDocumentModel(lifetime, protocol, document, documentModel.t4RdDocumentModel)
     }
   }
 }
