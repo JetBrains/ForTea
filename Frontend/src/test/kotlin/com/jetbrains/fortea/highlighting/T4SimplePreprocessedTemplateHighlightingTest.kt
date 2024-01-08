cfee00c34d75f052ca482bffc1d9df40780ae8cd
Nikita.Lyubimov <nikita.lyubimov@jetbrains.com>
2024-01-08 09:08:37 +0100

    [RIDER] do not check checkSolutionLoad property in T4SimplePreprocessedTemplateHighlightingTest
    GitOrigin-RevId: 31436bb372dacf9d2151fce03e352013211141bf

--- Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4SimplePreprocessedTemplateHighlightingTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/highlighting/T4SimplePreprocessedTemplateHighlightingTest.kt
@@ -4,6 +4,7 @@ import org.testng.annotations.Test
 
 class T4SimplePreprocessedTemplateHighlightingTest : T4HighlightingTestBase() {
   override fun getSolutionDirectoryName() = "ProjectWithSimplePreprocessedT4"
+  override val checkSolutionLoad = false
 
   @Test
   fun testVisibility() = doTestErrors()
