02096eeb0b866de225088c1d7a5edcdf6da5fbd0
Arseny Chernyaev <arseny.chernyaev@jetbrains.com>
2024-01-18 11:17:33 +0100

    introduce AsyncRunConfiguration.getRunProfileStateAsync  + clean up
    GitOrigin-RevId: b00da355c3345654f7591024f72024b94df21671

--- Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/run/T4RunConfiguration.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/run/T4RunConfiguration.kt
@@ -4,11 +4,10 @@ import com.intellij.execution.Executor
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.rd.util.startOnUiAsync
+import com.intellij.openapi.rd.util.withUiContext
 import com.jetbrains.fortea.configuration.run.execution.T4ExecutorFactory
-import com.jetbrains.rd.platform.util.lifetime
-import com.jetbrains.rd.platform.util.toPromise
 import com.jetbrains.rider.debugger.IRiderDebuggable
+import com.jetbrains.rider.run.RiderRunBundle
 import com.jetbrains.rider.run.configurations.AsyncRunConfiguration
 import com.jetbrains.rider.run.configurations.RiderRunConfiguration
 import org.jetbrains.concurrency.Promise
@@ -24,9 +23,17 @@ class T4RunConfiguration(
   { throw UnsupportedOperationException() },
   T4ExecutorFactory(project, parameters)
 ), IRiderDebuggable, AsyncRunConfiguration {
-  override fun getStateAsync(executor: Executor, environment: ExecutionEnvironment): Promise<RunProfileState> {
-    return environment.project.lifetime.startOnUiAsync {
+
+  override suspend fun getRunProfileStateAsync(executor: Executor, environment: ExecutionEnvironment): RunProfileState {
+    return withUiContext {
       (executorFactory as T4ExecutorFactory).createAsync(executor.id, environment)
-    }.toPromise()
+    }
+  }
+
+  @Suppress("UsagesOfObsoleteApi")
+  @Deprecated("Please, override 'getRunProfileStateAsync' instead")
+  override fun getStateAsync(executor: Executor, environment: ExecutionEnvironment): Promise<RunProfileState> {
+    @Suppress("DEPRECATION")
+    throw UnsupportedOperationException(RiderRunBundle.message("obsolete.synchronous.api.is.used.message", T4RunConfiguration::getStateAsync.name))
   }
 }
