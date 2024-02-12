d24e0986b50be920d0a5767182f4b6a1bbd27990
Alexander Karaev <Alexander.Karaev@jetbrains.com>
2024-02-12 11:34:20 +0200

    [radler] CPP-37193: Rename `IRiderFeatureZone` -> `IRiderBackendFeatureZone`
    * Preparation for splitting Rider and CLion (Radler)
    
    GitOrigin-RevId: 66f5ab6d6b1e28e2f308644995e426291eef50dc

--- Backend/RiderPlugin/ForTea.RiderPlugin/Psi/Resolve/Macros/FeatureAware/ZoneMarker.cs
+++ Backend/RiderPlugin/ForTea.RiderPlugin/Psi/Resolve/Macros/FeatureAware/ZoneMarker.cs
@@ -5,7 +5,7 @@ using JetBrains.Rider.Backend.Product;
 namespace JetBrains.ForTea.RiderPlugin.Psi.Resolve.Macros.FeatureAware
 {
   [ZoneMarker]
-  public class ZoneMarker : IRequire<IRiderFeatureZone>, IRequire<IRiderProductEnvironmentZone>
+  public class ZoneMarker : IRequire<IRiderBackendFeatureZone>, IRequire<IRiderProductEnvironmentZone>
   {
   }
 }
