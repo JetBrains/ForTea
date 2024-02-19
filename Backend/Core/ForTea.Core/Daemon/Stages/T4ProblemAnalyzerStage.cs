42bdc0b700d84c4c51c21c761e12fce6e5ee86ca
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2024-02-19 21:52:22 +0100

    Fix stage dependency asserts
    GitOrigin-RevId: 2a9f17c9ab26c2a31899e589617336d463161942

--- Backend/Core/ForTea.Core/Daemon/Stages/T4ProblemAnalyzerStage.cs
+++ Backend/Core/ForTea.Core/Daemon/Stages/T4ProblemAnalyzerStage.cs
@@ -6,7 +6,8 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 
 namespace GammaJul.ForTea.Core.Daemon.Stages
 {
-  [DaemonStage]
+  [DaemonStage(
+    StagesBefore = new[] { typeof(GlobalFileStructureCollectorStage) })]
   public class T4ProblemAnalyzerStage : T4DaemonStageBase
   {
     [NotNull] private ElementProblemAnalyzerRegistrar Registrar { get; }
