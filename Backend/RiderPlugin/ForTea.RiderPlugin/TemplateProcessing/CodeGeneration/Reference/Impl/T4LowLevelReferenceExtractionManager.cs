ea011b435a4e6c27ae312f4ad2e04493e4a77fe2
Nikita Popov <nikita.popov@jetbrains.com>
2024-09-04 14:11:04 +0200

    [dotnet] add support of new() calls for fields values and apply PrimaryToAnyThreadAttributesUpdateAction
    GitOrigin-RevId: 4fd552084d8d9fee489a9599dda1975c2bf87ffc

--- Backend/RiderPlugin/ForTea.RiderPlugin/TemplateProcessing/CodeGeneration/Reference/Impl/T4LowLevelReferenceExtractionManager.cs
+++ Backend/RiderPlugin/ForTea.RiderPlugin/TemplateProcessing/CodeGeneration/Reference/Impl/T4LowLevelReferenceExtractionManager.cs
@@ -18,7 +18,7 @@ using Microsoft.CodeAnalysis;
 
 namespace JetBrains.ForTea.RiderPlugin.TemplateProcessing.CodeGeneration.Reference.Impl
 {
-  [SolutionComponent(InstantiationEx.LegacyDefault)]
+  [SolutionComponent(Instantiation.DemandAnyThreadUnsafe)]
   public sealed class T4LowLevelReferenceExtractionManager : IT4LowLevelReferenceExtractionManager
   {
     [NotNull] private Dictionary<string, WeakReference<MetadataReference>> MetadataReferencesCache { get; }
