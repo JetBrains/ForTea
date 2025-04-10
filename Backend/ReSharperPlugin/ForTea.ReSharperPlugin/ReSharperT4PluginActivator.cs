a0e01a6f831e46b74da94be618feed66e82d2f24
Alexander Ulitin <alexander.ulitin@jetbrains.com>
2025-04-10 16:55:05 +0200

    [resharper, oop] don't activate T4/Unity on frontend side in OOP mode
    GitOrigin-RevId: 27d4ad5c14bc14fed34c9cf3df60ad64d71d9be7

--- Backend/ReSharperPlugin/ForTea.ReSharperPlugin/ReSharperT4PluginActivator.cs
+++ Backend/ReSharperPlugin/ForTea.ReSharperPlugin/ReSharperT4PluginActivator.cs
@@ -1,15 +1,25 @@
 using GammaJul.ForTea.Core;
 using JetBrains.Application.BuildScript.Application.Zones;
 using JetBrains.Application.Environment;
-using JetBrains.Platform.VisualStudio.SinceVs10.Zones;
+using JetBrains.Platform.VisualStudio.Protocol.BuildScript;
+using JetBrains.VsIntegration.Env;
+using JetBrains.VsIntegration.Zones;
 
 #pragma warning disable CheckNamespace
 
-namespace JetBrains.ForTea.ReSharperPluginActivation
+namespace JetBrains.ForTea.ReSharperPluginActivation.InProcess
 {
   [ZoneActivator]
-  [ZoneMarker(typeof(ISinceVs10FrontEnvZone))]
-  public class ReSharperT4PluginActivator : IActivate<IT4Zone>
+  [ZoneMarker(typeof(IVisualStudioFrontendEnvZone))]
+  public class ReSharperT4PluginActivator(VisualStudioProtocolConnector protocolConnector) : IActivateDynamic<IT4Zone>
   {
+    bool IActivateDynamic<IT4Zone>.ActivatorEnabled() => !protocolConnector.IsOutOfProcess.Value;
   }
+}
+
+namespace JetBrains.ForTea.ReSharperPluginActivation.OutOfProcess
+{
+  [ZoneActivator]
+  [ZoneMarker(typeof(IVisualStudioBackendOutOfProcessEnvZone))]
+  public class ReSharperT4PluginActivator : IActivate<IT4Zone>;
 }
