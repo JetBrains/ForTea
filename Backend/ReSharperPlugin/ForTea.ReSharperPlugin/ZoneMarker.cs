65ec8773858573eca8725035d24436100fbfdc94
Alexander Ulitin <alexander.ulitin@jetbrains.com>
2025-04-29 18:35:58 +0200

    [resharper, oop] fix RSRP-500517 User-friendly way to enable OOP
    fix RSRP-500275 Total hang after Save&Restart in R# Settings
    
    GitOrigin-RevId: 4a29f8bc29d587de9831bb0ce3047e60619f16ec

--- Backend/ReSharperPlugin/ForTea.ReSharperPlugin/ZoneMarker.cs
+++ Backend/ReSharperPlugin/ForTea.ReSharperPlugin/ZoneMarker.cs
@@ -1,11 +1,11 @@
 using GammaJul.ForTea.Core;
 using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.Platform.VisualStudio.SinceVs10.Zones;
+using JetBrains.VsIntegration.Zones;
 
 namespace JetBrains.ForTea.ReSharperPlugin
 {
   [ZoneMarker]
-  public class ZoneMarker : IRequire<IT4Zone>, IRequire<ISinceVs10FrontEnvZone>
+  public class ZoneMarker : IRequire<IT4Zone>, IRequire<IVisualStudioFrontendEnvZone>
   {
   }
 }
