ee0598d25b9a612cd3d5b1908fa147bd894f31a5
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2025-07-10 01:55:42 +0200

    [dotnet] Massive code cleanup: get rid of non-ASCII characters from identifier names (including Greek, Ukrainian i, mistyped Russian о and с, etc); get rid of some [NotNull] over Lifetime struct; get rid of Hungarian notation in delegate parameter/variable names
    GitOrigin-RevId: cbdeb5c93045ca886be82207ef7077015ee24b82

--- Backend/RiderPlugin/ForTea.RiderPlugin/ProtocolAware/Highlighting/Impl/T4OutputExtensionChangeListenerEx.cs
+++ Backend/RiderPlugin/ForTea.RiderPlugin/ProtocolAware/Highlighting/Impl/T4OutputExtensionChangeListenerEx.cs
@@ -8,19 +8,23 @@ namespace JetBrains.ForTea.RiderPlugin.ProtocolAware.Highlighting.Impl
   public static class T4OutputExtensionChangeListenerEx
   {
     [NotNull] private static readonly Key<T4OutputExtensionChangeListener> T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY =
-      new Key<T4OutputExtensionChangeListener>("T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY");
+      new("T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY");
 
     public static void CreateOutputExtensionChangeListener(
-      [NotNull] this IDocument thіs,
+      [NotNull] this IDocument document,
       Lifetime lifetime,
-      [NotNull] T4OutputExtensionChangeListener listener
-    ) => lifetime.Bracket(
-      () => thіs.PutData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY, listener),
-      () => thіs.PutData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY, null)
-    );
+      [NotNull] T4OutputExtensionChangeListener listener)
+    {
+      lifetime.Bracket(
+        () => document.PutData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY, listener),
+        () => document.PutData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY, null)
+      );
+    }
 
     [CanBeNull]
-    public static T4OutputExtensionChangeListener GetOutputExtensionChangeListener([NotNull] this IDocument thіs) =>
-      thіs.GetData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY);
+    public static T4OutputExtensionChangeListener GetOutputExtensionChangeListener([NotNull] this IDocument document)
+    {
+      return document.GetData(T4_OUTPUT_EXTENSION_CHANGE_LISTENER_KEY);
+    }
   }
 }
