3aae637b7cacb23e0ea4d62047307171a9c07168
Alexander.Shvedov <alexander.shvedov@jetbrains.com>
2025-11-24 18:22:09 +0100

    RIDER-128433 Rider .NET Intentions Previews
    GitOrigin-RevId: 268fc25f553a88318d6cd063aa905e069a87bf84

--- Backend/RiderPlugin/ForTea.RiderPlugin/Daemon/Syntax/T4RiderSyntaxHighlightingManager.cs
+++ Backend/RiderPlugin/ForTea.RiderPlugin/Daemon/Syntax/T4RiderSyntaxHighlightingManager.cs
@@ -1,13 +1,16 @@
 using GammaJul.ForTea.Core.Psi;
 using JetBrains.ReSharper.Daemon.Syntax;
-using JetBrains.ReSharper.Daemon.SyntaxHighlighting;
 using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.Tree;
 
 namespace JetBrains.ForTea.RiderPlugin.Daemon.Syntax
 {
   [Language(typeof(T4Language))]
   public class T4SyntaxHighlightingManager : SyntaxHighlightingManager
   {
-    public override SyntaxHighlightingProcessor CreateProcessor() => new T4SyntaxHighlightingProcessor();
+    public override SyntaxHighlightingProcessor CreateProcessor(IPsiSourceFile sourceFile, IFile psiFile)
+    {
+      return new T4SyntaxHighlightingProcessor();
+    }
   }
 }
