cd06422c58586010619e5b84e5d1e22ec0871e63
andrew.karpov <andrew.karpov@jetbrains.com>
2025-12-22 17:53:50 +0100

    RSRP-502572 Reduce the perf impact of the type-checking
    WIP
    
    Pass type of element into `NodeType` constructor
    to calculate node type flags
    and build map from implemented interface types to node type indicies.
    
    GitOrigin-RevId: 2fd49a9f1e6633856c0f9d30e17f5d64d00f6f55

--- Backend/Core/ForTea.Core/Tree/Impl/T4CompositeNodeType.cs
+++ Backend/Core/ForTea.Core/Tree/Impl/T4CompositeNodeType.cs
@@ -1,10 +1,11 @@
+using System;
 using JetBrains.ReSharper.Psi.ExtensionsAPI.Tree;
 
 namespace GammaJul.ForTea.Core.Tree.Impl
 {
   public abstract class T4CompositeNodeType : CompositeNodeType
   {
-    protected T4CompositeNodeType(string s, int index, NodeTypeFlags flags) : base(s, index, flags)
+    protected T4CompositeNodeType(string s, int index, Type nodeType) : base(s, index, nodeType)
     {
     }
   }
