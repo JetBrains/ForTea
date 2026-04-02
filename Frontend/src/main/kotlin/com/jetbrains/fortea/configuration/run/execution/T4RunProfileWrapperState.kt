3f8593fc3da367b7d59f223d3df0bd3c2c8d9936
Sergey.Vardanyan <sergey.vardanyan@jetbrains.com>
2026-04-02 15:43:51 +0400

    Fix RIDER-137048: Add suspending methods to PatchCommandLineExtension
    - Replaced promise-based `patchDebugCommandLine` with `patchDebugCommandLineSuspending` to align with coroutine-based method standards.
    - Updated usages across the codebase and added appropriate `suspend` and `@Deprecated` annotations.
    - Refactored implementation for better error-resilient async handling.
    
    RIDER-137048: Migrate to async execution in IDotNetProfileState
    
    RIDER-137048: Add async execution in T4RunProfileWrapperState
    
    GitOrigin-RevId: 65cfa312af03519fd9c87c3c92db11f6f6d26037

--- Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/run/execution/T4RunProfileWrapperState.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/configuration/run/execution/T4RunProfileWrapperState.kt
@@ -4,18 +4,37 @@ import com.intellij.execution.ExecutionResult
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ProgramRunner
+import com.intellij.openapi.diagnostic.thisLogger
 import com.jetbrains.fortea.configuration.run.T4RunConfigurationParameters
 import com.jetbrains.fortea.model.T4ProtocolModel
+import com.jetbrains.rider.run.configurations.RiderAsyncRunProfileState
 
 class T4RunProfileWrapperState(
   private val wrappee: RunProfileState,
   private val model: T4ProtocolModel,
   private val parameters: T4RunConfigurationParameters
-) : RunProfileState {
+) : RunProfileState, RiderAsyncRunProfileState {
+
+  @Deprecated("Use executeAsync instead")
   override fun execute(executor: Executor?, runner: ProgramRunner<*>): ExecutionResult? {
+    thisLogger().error("T4RunProfileWrapperState.execute is deprecated. Use executeAsync instead")
+    return executeWithListener { wrappee.execute(executor, runner) }
+  }
+
+  override suspend fun executeAsync(
+    executor: Executor,
+    runner: ProgramRunner<*>
+  ): ExecutionResult {
+    return executeWithListener {
+      if(wrappee is RiderAsyncRunProfileState) wrappee.executeAsync(executor, runner)
+      else wrappee.execute(executor, runner)
+    } ?: throw IllegalStateException("wrappee.execute returned null")
+  }
+
+  private  inline fun executeWithListener(executeWrapee: () -> ExecutionResult?) : ExecutionResult? {
     val listener = T4PostProcessorProcessListener(model, parameters)
     try {
-      val result: ExecutionResult? = wrappee.execute(executor, runner)
+      val result: ExecutionResult? = executeWrapee()
       if (result == null) {
         listener.notifyBackendAboutProcessCompletion(false)
         return null
