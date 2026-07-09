c596ee35a8e1c2ae4fb921b4f3b9b2aea9a9aa21
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2026-07-09 14:56:33 +0300

    RIDER-140455 [rider] Fix return value in tests
    GitOrigin-RevId: 6a76eb366f7548a07fea40c1716278dcb88f2916

--- Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4ExecutableOutputDependentLexerTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4ExecutableOutputDependentLexerTest.kt
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test
 class T4ExecutableOutputDependentLexerTest : T4OutputDependentLexerTestBase() {
   override val testSolution = "ProjectWithT4"
 
-  @Test fun `test basic C# highlighting`() = doTest()
-  @Test fun `test basic HTML highlighting`() = doTest()
-  @Test fun `test that extension can contain dot`() = doTest()
-  @Test fun `test default extension in executable template`() = doTest()
+  @Test fun `test basic C# highlighting`() { doTest() }
+  @Test fun `test basic HTML highlighting`() { doTest() }
+  @Test fun `test that extension can contain dot`() { doTest() }
+  @Test fun `test default extension in executable template`() { doTest() }
 }
