23c3890282a35249c2a59a913aa9504ac3fa8076
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2026-07-09 15:11:37 +0300

    RIDER-140455 [rider] Fix test
    GitOrigin-RevId: e4337b318dc4cd922c8cf0b3e01d91b0133f370e

--- Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4PreprocessedOutputDependentLexerTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4PreprocessedOutputDependentLexerTest.kt
@@ -1,12 +1,15 @@
 package com.jetbrains.fortea.lexer
 
 import com.jetbrains.fortea.Tags
+import com.jetbrains.rider.test.annotations.Solution
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 
 @Tag(Tags.Episode.ForTea)
+@Solution("ProjectWithSimplePreprocessedT4")
 class T4PreprocessedOutputDependentLexerTest : T4OutputDependentLexerTestBase() {
   override val testSolution = "ProjectWithSimplePreprocessedT4"
+  override val checkSolutionLoad = false
 
   @Test fun `test default extension in preprocessed template`() { doTest() }
 }
