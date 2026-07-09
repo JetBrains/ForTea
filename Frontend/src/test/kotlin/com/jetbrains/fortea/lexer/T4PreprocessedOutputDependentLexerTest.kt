80ab5570f4327846ccb67da680a545e6400b0a0e
Konstantin Krukhmalev <konstantin.krukhmalev@jetbrains.com>
2026-07-09 14:56:33 +0300

    RIDER-140455 [rider] Fix return value in tests
    GitOrigin-RevId: 6a76eb366f7548a07fea40c1716278dcb88f2916

--- Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4PreprocessedOutputDependentLexerTest.kt
+++ Frontend/src/test/kotlin/com/jetbrains/fortea/lexer/T4PreprocessedOutputDependentLexerTest.kt
@@ -8,5 +8,5 @@ import org.junit.jupiter.api.Test
 class T4PreprocessedOutputDependentLexerTest : T4OutputDependentLexerTestBase() {
   override val testSolution = "ProjectWithSimplePreprocessedT4"
 
-  @Test fun `test default extension in preprocessed template`() = doTest()
+  @Test fun `test default extension in preprocessed template`() { doTest() }
 }
